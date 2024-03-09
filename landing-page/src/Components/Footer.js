9c4ecddd20a21d0256508321fe33decb04a2bcfe
JonathanSDP <jonas1993seba@gmail.com>
2024-03-08 22:04:09 -0300

    Landing Page Actualización2

--- landing-page/src/Components/Footer.js
+++ landing-page/src/Components/Footer.js
@@ -10,8 +10,8 @@ const Footer = () => {
             </h2>
             <div className='img-footer-container'>
                 <a href="#" className='icon1'><img src={require("../Assets/holajr.png")}/></a>
-                <a href="#" className='icon2'><img src={require("../Assets/linkedin.png")}/></a>
-                <a href="#" className='icon3'><img src={require("../Assets/github.png")}/></a>
+                <a href="https://www.linkedin.com/in/jonathan-pojomovsky-94a474296" className='icon2'><img src={require("../Assets/linkedin.png")}/></a>
+                <a href="https://github.com/JonathanSDP" className='icon3'><img src={require("../Assets/github.png")}/></a>
             </div>
             <span className='copy'>Copyright© 2024. Jonathan Pojomovsky</span>
         </footer>
